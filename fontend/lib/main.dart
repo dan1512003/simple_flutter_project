586d668aaa01fbe634c3292de3a4cac3beabb20d
dan1512003 <nnguyengg123@gmail.com>
2024-09-19 17:23:53 +0700

    Localhost cho android

--- fontend/lib/main.dart
+++ fontend/lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -27,16 +29,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final controller = TextEditingController();
   String responseMessage = '';
+  String getBackendUrl() {
+    if (kIsWeb) {
+      return 'http://localhost:8080';
+    } else if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    } else {
+      return 'http://localhost:8080';
+    }
+  }
+
   Future<void> senName() async {
     final name = controller.text;
     controller.clear();
-    final url = Uri.parse('http://localhost:8080/api/v1/submit');
+    final backendUrl = getBackendUrl();
+    final url = Uri.parse('$backendUrl/api/v1/submit');
     try {
-      final response = await http.post(
-        url,
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode({'name': name}),
-      ) .timeout(const Duration(seconds: 10));
+      final response = await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({'name': name}),
+          )
+          .timeout(const Duration(seconds: 10));
       if (response.body.isNotEmpty) {
         final data = json.decode(response.body);
         setState(() {
